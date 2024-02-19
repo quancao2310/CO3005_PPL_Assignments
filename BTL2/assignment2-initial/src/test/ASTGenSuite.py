93c961b248b2b3c9db08604c9c07e3c6bba0f580
Quan Cao <quancao2310@gmail.com>
2024-02-19 23:20:21 +0700

    Assignment 2: Add AST Generation

--- BTL2/assignment2-initial/src/test/ASTGenSuite.py
+++ BTL2/assignment2-initial/src/test/ASTGenSuite.py
@@ -8,8 +8,15 @@ class ASTGenSuite(unittest.TestCase):
     * 300-399: 
     """
     
-    def test_simple_program(self):
+    def test_300(self):
         input = """number a
         """
         expect = str(Program([VarDecl(Id("a"), NumberType())]))
         self.assertTrue(TestAST.test(input, expect, 300))
+    
+    def test_301(self):
+        input = """func foo() begin
+end
+"""
+        expect = str(Program([FuncDecl(Id("foo"), [], Block([]))]))
+        self.assertTrue(TestAST.test(input, expect, 301))
